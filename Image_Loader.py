823df1b80de31bb7b273af0082db9d6804afb628
Sarthak Bachhar <Sarthak2.Bachhar@live.uwe.ac.uk>
2024-09-06 12:32:19 +0545

    If-Else condition was added to check if the image was successfully loaded

--- Image_Loader.py
+++ Image_Loader.py
@@ -3,8 +3,12 @@ import cv2
 # Load the image  
 img = cv2.imread("C:\\Users\\Acer\\Desktop\\nobi.jpg")
 
-# Display the image 
-cv2.imshow("Cartoon", img)
+# Check if the image was successfully loaded 
+if img is None:
+  print("Unable to load image.")
+else:
+  # Display the image 
+  cv2.imshow("Cartoon", img)
 
 # To display until a key is pressed
 cv2.waitKey(0)
